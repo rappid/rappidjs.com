c65fbf4124aeae3ba058451f51a801a9aa301a49
Tony Findeisen <github@it-ony.de>
2012-06-24 22:45:21 +0200

    added documentation

--- public/app/module/DocumentationClass.js
+++ public/app/module/DocumentationClass.js
@@ -1,10 +1,34 @@
-define(['js/core/Module', "json!doc/index"], function(Module, index) {
+define(['js/core/Module', "json!doc/index", "js/core/List", "documentation/model/Class"], function(Module, classIndex, List, Class) {
 
     return Module.inherit("app.module.DocumentationClass", {
 
-        initialize: function() {
-            this.callBase();
-            this.set('classes', index);
-        }
+        defaults: {
+            classes: List,
+            doc: null
+        },
+
+        _initializationComplete: function() {
+            for (var i = 0; i < classIndex.length; i++) {
+                this.$.classes.add(this.$.api.createEntity(Class, classIndex[i]));
+            }
+        },
+
+        hasCurrentDocumentation: function() {
+            return !!this.$.doc;
+        }.onChange('doc'),
+
+        showClass: function(routeContext, fqClassName) {
+
+            var self = this;
+            this.$.api.createEntity(Class, fqClassName).fetch(null, function(err, classDoc) {
+                self.set('doc', classDoc);
+                routeContext.callback(err);
+
+                if (self.runsInBrowser()) {
+                    window.scrollTo(window.scrollX, 0);
+                }
+            })
+
+        }.async()
     });
 });
