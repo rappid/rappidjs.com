fbbc3086d9790ff304624548fea563a24e8c7a66
Tony Findeisen <github@it-ony.de>
2012-08-05 20:33:44 +0200

    implemented Demonstration for DataGrid

--- public/app/module/UiClass.js
+++ public/app/module/UiClass.js
@@ -11,10 +11,15 @@ define(['js/core/Module', 'sprd/model/Shop'], function (Module, Shop) {
 
             if (!this.$.shop) {
                 var shop = this.$.api.createEntity(Shop, 205909);
+                shop.fetch();
                 this.set("shop", shop);
             }
 
             this.callBase();
+        },
+
+        visit: function(e) {
+            // TODO: http://www.spreadshirt.net/-C4408A{data.id}
         }
     });
 });
