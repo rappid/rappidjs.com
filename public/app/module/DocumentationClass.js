e0f30234ceaaf09fcbb1a9880c3b44fa11c452ea
Tony Findeisen <github@it-ony.de>
2013-04-27 12:15:35 +0200

    check public and protected methods of documentation for node rendering

--- public/app/module/DocumentationClass.js
+++ public/app/module/DocumentationClass.js
@@ -8,6 +8,7 @@ define(['js/core/Module', "json!doc/index.json", "js/core/List", "documentation/
             searchString: ""
         },
 
+
         showTypes: function(showPublic, showProtected) {
 
             var type = (showPublic ? 2 : 0) + (showProtected ? 1 : 0);
@@ -20,7 +21,26 @@ define(['js/core/Module', "json!doc/index.json", "js/core/List", "documentation/
             for (var i = 0; i < classIndex.length; i++) {
                 this.$.classes.add(this.$.api.createEntity(Class, classIndex[i]));
             }
+
+            if (!this.runsInBrowser()) {
+                var checked = {
+                    checked: true
+                };
+
+                this.$.showPublic.set(checked);
+                this.$.showProtected.set(checked);
+
+                this.$.showPublicStatic.set(checked);
+                this.$.showProtectedStatic.set(checked);
+
+                this.$.publicAttributesCheckbox.set(checked);
+                this.$.protectedAttributesCheckbox.set(checked);
+
+            }
+
+            this.callBase();
         },
+
         initialize: function(){
             this.set('filterDocs', function(doc, index, filterList){
                 return !filterList.$.searchString || doc.$.id.toLowerCase().indexOf(filterList.$.searchString.toLowerCase()) > -1;
