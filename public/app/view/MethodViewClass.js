23cb5fcdda390cb04e6ff84ba29d8a146f25b2b3
Tony Findeisen <github@it-ony.de>
2013-05-04 16:20:15 +0200

    formatted MethodViewClass

--- public/app/view/MethodViewClass.js
+++ public/app/view/MethodViewClass.js
@@ -1,21 +1,18 @@
-define(
-    ["js/ui/View"], function (View) {
+define(["js/ui/View"], function (View) {
 
+    return View.inherit({
+        defaults: {
+            method: null
+        },
 
-        return View.inherit({
-            defaults: {
-                method: null
-            },
+        methodCSSClasses: function () {
+            var ret = [this.$.method.$.visibility];
 
-            methodCSSClasses: function(){
-                var ret = [this.$.method.$.visibility];
-
-                if(this.$.method.$.hasOwnProperty('definedBy')){
-                    ret.push("inherited");
-                }
-
-                return ret.join(" ");
+            if (this.$.method.$.hasOwnProperty('definedBy')) {
+                ret.push("inherited");
             }
-        });
-    }
-);
+
+            return ret.join(" ");
+        }
+    });
+});
