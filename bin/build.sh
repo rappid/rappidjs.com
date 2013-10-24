b4ab32bfe3bd61267192f78676e34396216ce584
Tony Findeisen <github@it-ony.de>
2013-10-24 18:00:50 +0200

    added initial build script for rappidjs.com site

--- bin/build.sh
+++ bin/build.sh
@@ -4,8 +4,16 @@
 set -e
 
 WORKSPACE=`pwd`
-DEPENDENCIES="$WORKSPACE/dependencies"
+DEPENDENCIES="${WORKSPACE}/dependencies"
 REPOSITORY=/local/repository/rappidjs.com
+VERSION=${BUILD_NUMBER}
+BRANCH=${GIT_BRANCH};
+REPO=${REPOSITORY}/${VERSION};
+
+if [ -z BRANCH ]; then
+    # branch not set from jenkins, get it from the git commandline
+    BRANCH=`git status | grep "On branch" | cut -d' '  -f 4`
+fi
 
 rm -rf public public-build
 git reset --hard HEAD
@@ -13,12 +21,12 @@ git pull origin dev;
 
 # npm install
 
-echo "Dependencies: $DEPENDENCIES"
+echo "Dependencies: ${DEPENDENCIES}"
 
-rm -rf $DEPENDENCIES
-mkdir $DEPENDENCIES
+rm -rf ${DEPENDENCIES}
+mkdir ${DEPENDENCIES}
 
-cd $DEPENDENCIES
+cd ${DEPENDENCIES}
 
 git clone git://github.com/rappid/rAppid.js.git
 cd rAppid.js
@@ -26,39 +34,73 @@ git checkout dev
 
 npm install --production
 
-cd $DEPENDENCIES
+cd ${DEPENDENCIES}
 
 git clone https://github.com/spreadshirt/rAppid.js-sprd.git
 cd rAppid.js-sprd
 git checkout dev
 
-cd $DEPENDENCIES
+cd ${DEPENDENCIES}
 
 git clone https://github.com/rappid/rappidjs-piwik.git
 
-cd $WORKSPACE/public
+cd ${WORKSPACE}/public
 
-ln -s $DEPENDENCIES/rAppid.js/js
-ln -s $DEPENDENCIES/rAppid.js-sprd/sprd
-ln -s $DEPENDENCIES/rappidjs-piwik/piwik
+ln -s ${DEPENDENCIES}/rAppid.js/js
+ln -s ${DEPENDENCIES}/rAppid.js-sprd/sprd
+ln -s ${DEPENDENCIES}/rappidjs-piwik/piwik
 
-cd $WORKSPACE
+cd ${WORKSPACE}/server
+ln -s ${DEPENDENCIES}/rAppid.js/js
+ln -s ${DEPENDENCIES}/rAppid.js/srv
+ln -s ../public/app
 
-RAPPIDJS="$DEPENDENCIES/rAppid.js/bin/rappidjs"
-chmod +x $RAPPIDJS
+cd ${WORKSPACE}
+
+RAPPIDJS="${DEPENDENCIES}/rAppid.js/bin/rappidjs"
+chmod +x ${RAPPIDJS}
 
 VERSION="`$RAPPIDJS version`-$BUILD_NUMBER";
 
 echo "VERSION: $VERSION"
 
-$RAPPIDJS build --version $VERSION
+$RAPPIDJS build --version ${VERSION}
 
 cd public-build/
-cp index.html $VERSION/
+cp index.html ${VERSION}/
 
-REPO=$REPOSITORY/$VERSION;
-mkdir -p $REPO;
+mkdir -p ${REPO};
+tar -czf ${REPO}/client.tar.gz index.html ${VERSION}
 
-tar -czf $REPO/client.tar.gz index.html $VERSION
+rm -f ${REPOSITORY}/latest
+ln -s ${REPO} ${REPOSITORY}/latest
 
-echo $VERSION > /local/version/rappidjs.com.version
+cd ${WORKSPACE}
+rm -rf tmp
+mkdir tmp
+
+cp -r -L server tmp
+cp -r ${DEPENDENCIES}/rAppid.js tmp
+cp -r public/ tmp/
+cp index.js tmp
+mv node_modules/ tmp/
+
+cd tmp/public
+rm -f js
+ln -s ../rAppid.js/js
+
+cd ${WORKSPACE}/tmp/server
+rm -rf js srv
+ln -s ../rAppid.js/js
+ln -s ../rAppid.js/srv
+
+cd ${WORKSPACE}
+
+tar -czf ${REPO}/server.tar.gz -C ${WORKSPACE}/tmp .
+
+
+echo ${VERSION} > /local/version/rappidjs.com.version
+
+if [ ${USER} == "jenkins" ]; then
+    sudo /local/www/rappidjs/bin/update beta ${VERSION}
+fi
